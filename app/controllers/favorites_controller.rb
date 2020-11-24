a6d0c7aa5096eccc89f1fa19b39d1db16326bffb
masa-8432 <masachi323@gmail.com>
2020-11-24 10:26:24 +0000

    コメント非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,22 +6,15 @@ class FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: @post.id)
     @posts = Post.all
-    respond_to do |format|
-      if favorite.save
-        # format.js {render :template => "favorites/index", locals: {post: @post}}
-        format.js { render locals: { post: @post } }
-      end
-    end
+    favorite.save
+
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: @post.id)
     @posts = Post.all
-    respond_to do |format|
-      if favorite.destroy
-        format.js { render locals: { post: @post } }
-      end
-    end
+    favorite.destroy
   end
+
 end
